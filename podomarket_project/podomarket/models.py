575febc324ee84b74b7f4012fdef55180b03f49d
KeonHee Lee <power3869@gmail.com>
2021-10-23 19:48:38 +0900

    [UserTable]append new fields to User Table

--- podomarket_project/podomarket/models.py
+++ podomarket_project/podomarket/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    nickname = models.CharField(max_length=15, unique=True, null=True)
+    kakao_id = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=40, null=True)
+
     def __str__(self):
         return self.email
